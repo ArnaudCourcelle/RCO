7e4f5515ef4142f30127d3d33deea54f277a1b1c
jipsy <jean-philippe.riviere@lri.fr>
2018-03-26 17:50:47 +0200

    changement port test

--- RCOServeurLastVersion2/public/new/PD/testRCO.js
+++ RCOServeurLastVersion2/public/new/PD/testRCO.js
@@ -4,7 +4,7 @@
 //var socket = require('socket.io-client')('http://rco.fr');
 //var socket = require('socket.io-client')('http://127.0.0.1:8080/');
 //var socket = require('socket.io-client')('http://localhost:8080');
-var socket = require('socket.io-client')('https://rco.lri.fr:8080');
+var socket = require('socket.io-client')('https://rco.lri.fr:8081');
 
 //var socket = require('socket.io-emitter')({ host: 'localhost', port: 8080 });
 var osc=require('node-osc');
